0a0f6dae6a815aa6edba967af2d443c52ba0bf11
Souravkumarverma123 <souravkumarverma56@gmail.com>
2026-06-07 17:49:06 +0530

    fix: update root route response to return a JSON message indicating server health

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000; 
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  return res.json({ message: "server is healthy and running fine" });
 });
 
 app.listen(PORT, () => {
