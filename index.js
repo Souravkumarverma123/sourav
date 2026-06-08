e87da6b18127c2aa8e8d70920116a830c417b9cd
Souravkumarverma123 <souravkumarverma56@gmail.com>
2026-06-08 12:30:15 +0530

    fix: update root route response message to indicate changes have been made

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT ?? 8000; 
 
 app.get("/", (req, res) => {
-  return res.json({ message: "server is healthy and running fine" });
+  return res.json({ message: "server is healthy and running fine and changes are done " });
 });
 
 app.listen(PORT, () => {
