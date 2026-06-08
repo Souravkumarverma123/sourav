bd2ec027cd0a4dd387f213ae4197b5fa91b8f435
Souravkumarverma123 <souravkumarverma56@gmail.com>
2026-06-08 19:30:57 +0530

    fix: correct welcome message in root route response

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 const PORT = process.env.PORT ?? 8000; 
 
 app.get("/", (req, res) => {
+  return res.json({ message: "Welocome to my server" });
+});
+
+app.get("/health", (req, res) => {
   return res.json({ message: "server is healthy and running 🐋  ", status: "Github actions implemented" });
 });
 
